101cb299c997f267360ed033972d851e9735db57
Adrián Meizoso <adrian.meizosolopez@altran.com>
2018-12-17 15:47:29 +0100

    Deleted New Group folder

---
+++ NVSPack/ViewLayer/Controllers/Home/HomePresenter/View/HomeViewContent.swift
@@ -0,0 +1,34 @@
+//
+//  HomeViewContent.swift
+//  NVSPack
+//
+//  Created by 08APO0516 on 12/12/2018.
+//  Copyright © 2018 jca. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewContent: UITableViewController {
+
+    // MARK: - Private attributes
+    //private var timeSelectorPresenter : TimeSelectorPresenter!
+
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.allowsSelection = false
+        // Do any additional setup after loading the view.
+    }
+    
+/*
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+        if segue.identifier == R.segue.homeViewContent.timeSelectorPresenterSegue.identifier,
+           let timeSelectorPresenter = segue.destination as? TimeSelectorPresenter {
+            timeSelectorPresenter.onSelectedPeriod = { timeSelector in
+                 weakSelf.onSelectedPeriod(timeSelector)
+            }
+        }
+    }
+*/
+}
